4dacb0853f6996f34c3478e96c253eb1ce637f1e
Holajuwon <henrijay00@gmail.com>
2020-04-05 03:24:59 +0100

    ch-fix-conection-error

--- config/routes.js
+++ config/routes.js
@@ -2,13 +2,16 @@ const bodyParser = require("body-parser");
 const post = require("../routes/post");
 const comment = require("../routes/comments");
 const triangle = require("../routes/triangle");
-const cors = require('cors')
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
 
-
-module.exports = app => {
-  app.use(cors())
+module.exports = (app) => {
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(compression());
+  app.use(helmet());
   app.use("/api/post", post);
   app.use("/api/comment", comment);
   app.use("/api/triangle", triangle);
