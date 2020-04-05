5932d1e7851e6382618b0072fd2a2de18d6f4513
Holajuwon <henrijay00@gmail.com>
2020-04-05 02:33:58 +0100

    ch-fix-conection-error

--- elephantsql.js
+++ elephantsql.js
@@ -1,14 +1,13 @@
 var { Pool } = require("pg");
 require("dotenv").config();
 
-const conObj = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-};
+const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new Pool(process.env.DATABASE_URL || conObj);
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
 
 module.exports = pool;
