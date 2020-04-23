a3a0e3c1abb159ab0e28055c529e10512d7f08c8
Holajuwon <henrijay00@gmail.com>
2020-04-23 04:07:35 +0100

    modified the schema(init.sql)

--- init.sql
+++ init.sql
@@ -1,4 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+DROP TABLE IF EXISTS user_posts CASCADE;
+CREATE TABLE IF NOT EXISTS user_posts
+(
+    post_content character varying(10000) COLLATE pg_catalog."default" NOT NULL,
+    author character varying(255) COLLATE pg_catalog."default" DEFAULT 'Anonymous'::character varying,
+    post_id uuid NOT NULL DEFAULT uuid_generate_v1(),
+    post_date character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    post_title character varying(500) COLLATE pg_catalog."default" NOT NULL DEFAULT 'A Beautiful Post'::character varying,
+    image_url "char",
+    CONSTRAINT user_posts_pkey PRIMARY KEY (post_id),
+    CONSTRAINT user_posts_post_id_key UNIQUE (post_id)
+);
+
+DROP TABLE IF EXISTS user_comments CASCADE;
 CREATE TABLE IF NOT EXISTS user_comments
 (
     user_comment character varying(10000) COLLATE pg_catalog."default" DEFAULT 'Anonymous'::character varying,
@@ -13,14 +27,3 @@ CREATE TABLE IF NOT EXISTS user_comments
         ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS user_posts
-(
-    post_content character varying(10000) COLLATE pg_catalog."default" NOT NULL,
-    author character varying(255) COLLATE pg_catalog."default" DEFAULT 'Anonymous'::character varying,
-    post_id uuid NOT NULL DEFAULT uuid_generate_v1(),
-    post_date character varying(32) COLLATE pg_catalog."default" NOT NULL,
-    post_title character varying(500) COLLATE pg_catalog."default" NOT NULL DEFAULT 'A Beautiful Post'::character varying,
-    image_url "char",
-    CONSTRAINT user_posts_pkey PRIMARY KEY (post_id),
-    CONSTRAINT user_posts_post_id_key UNIQUE (post_id)
-);
